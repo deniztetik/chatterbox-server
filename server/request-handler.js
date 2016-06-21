ed8ed702b614f38df8a058b5c4bb7859f0d00f4c
MKS-students <mks.students@hackreactor.com>
2016-06-21 19:26:28 -0400

    completed all basic requirements, plus a bunch of other horrible stuff.

--- server/request-handler.js
+++ server/request-handler.js
@@ -15,102 +15,111 @@ this file and include it in basic-server.js so that it actually works.
 var _ = require('underscore');
 var results = require('./data.js').results;
 var rooms = require('./data.js').rooms;
+var fs = require('fs');
+var $ = require('jQuery');
+const StringDecoder = require('string_decoder').StringDecoder;
+const decoder = new StringDecoder ('utf8');
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+
+var defaultCorsHeaders = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
+};
 
 exports.requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
+  var headers = defaultCorsHeaders;
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
   console.log("Serving request type " + request.method + " for url " + request.url);
 
   // The outgoing status.
   var statusCode = 200;
 
+  var getAndSendFile = function(path, contentType) {
+    var content = '';
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        console.error('error!');
+      }
+      content += data;
+        response.writeHead(200, {'Content-Type': contentType});
+        response.end(content);
+      // });
+    });
+  }
+
+  if(request.method == 'GET') {
+    var data = {
+      results: []
+    };
+    var path = '';
+    var route = request.url.split('/');
+    route.shift();
+    var currentLocation = route.shift();
+    console.log('current location is: ', currentLocation);
+    // console.l
+    if (request.url === '/') {
+      path = 'client/index.html';
+      getAndSendFile(path, 'text/html');
+    } else if (request.url === '/?createdAt=-createdAt') {
+
+      console.log('GET request to "classes."')
+      data.results = results;
+      var validUrl = request.url.split('/');
+
+      // if (validUrl[1] !== 'classes') {
+      //   statusCode = 404;
+      // }
+
+      response.writeHead(statusCode, headers);
+      response.end(JSON.stringify(data));
+    } else if (currentLocation !== 'classes') {
+      path = __dirname.split('/');
+      path.pop();
+      path = path.join('/');
+      path += '/client' + request.url;
+      console.log('After all of this stupid shit, our path is:  ', path);
+      console.log(route);
+      if (route.length > 0) {
+        var fileType = route.pop().split('.');
+        fileType = fileType.pop();
+      }
+
+      if (fileType === 'css') {
+        getAndSendFile(path, 'text/css');
+      } else if (fileType === 'js') {
+        getAndSendFile(path, 'text/javascript')
+      }
+
+    } else {
+      statusCode = 404;
+      response.end();
+    }
+  }
+
+
   if(request.method == 'POST') {
-
-
     var room = request.url.split('/')[2];
     if (!_.contains(rooms, room)) {
       rooms.push(room);
     }
     statusCode = 201;
-
     var body = '';
     request.on('data', function(data) {
       body += data;
     });
-
+    console.log(body);
     request.on('end', function(data) {
       data = JSON.parse(body);
       if (!data.username) {
         data.username = 'Anon';
       }
       results.push(data);
+      response.end();
     });
   }
-
-  var data = {
-    results: []
-  };
-
-  var getTag = new RegExp('[/][?].*', 'g');
-
-  if(request.method == 'GET') {
-    data.results = results;
-
-    var validUrl = request.url.split('/')[1];
-
-    if (validUrl !== 'classes') {
-      statusCode = 404;
-    }
-  }
-
-  // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = "JSON";
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end(JSON.stringify(data));
-};
-
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
 };
 
